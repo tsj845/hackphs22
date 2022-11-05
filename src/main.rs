bb6a03f908d0fb8a6d6316bb3176e09fa2bb8334
Vihaan Jim <vjim@hs-student27612.princetonk12.org>
2022-11-05 13:44:12 -0400

    remove make func

--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,4 @@ fn main() {
     let argv: Vec<String> = Vec::from_iter(args());
     let toks = parse_input((&argv[1..].concat()).to_owned());
     println!("{:?}", toks);
-    println!("{}", to_latex(tree::make(toks)))
 }
