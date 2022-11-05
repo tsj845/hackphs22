c3f13aa7179ec8c4cac6b19faeca2459314aa0cd
Tristan <tschartman41@gmail.com>
2022-11-05 11:06:08 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use std::env::args;
 use text_processing::parse_input;
 
 fn main() {
-    let a: Vec<String> = Vec::from_iter(args());
-    println!("{:?}", a);
-    println!("{:?}", parse_input(a[1].clone()));
+    let argv: Vec<String> = Vec::from_iter(args());
+    println!("{:?}", parse_input(argv[1].clone()));
 }
