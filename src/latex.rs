4f7324ce34ac83a6d09891c220878a53536de44e
Vihaan Jim <vjim@hs-student27612.princetonk12.org>
2022-11-05 11:33:20 -0400

    push_str

--- src/latex.rs
+++ src/latex.rs
@@ -1,39 +1,44 @@
 use crate::tree::Operation;
 
 pub fn to_latex(tree: &Box<Operation>) -> String {
-    let latex = String::new();
+    let mut latex = String::new();
     match tree {
 	Operation::Num(a) => {
 	    latex += format!("{}", a);
 	}
 	Operation::Add(a, b) => {
-	    latex += to_latex(a);
-	    latex += "+";
-	    latex += to_latex(b);
+	    latex.push_str(&to_latex(a));
+	    latex.push_str("+");
+	    latex.push_str(&to_latex(b));
 	},
 	Operation::Subtract(a, b) => {
-	    latex += to_latex(a);
-	    latex += "-";
-	    latex += to_latex(b);
+	    latex.push_str(&to_latex(a));
+	    latex.push_str("-");
+	    latex.push_str(&to_latex(b));
 	},
 	Operation::Multiply(a, b) => {
-	    latex += to_latex(a);
-	    latex += " \times ";
-	    latex += to_latex(b);
+	    latex.push_str(&to_latex(a));
+	    latex.push_str(" \times ");
+	    latex.push_str(&to_latex(b));
 	},
 	Operation::Divide(a, b) => {
-	    latex += "\frac{";
-	    latex += to_latex(a);
-	    latex += "}{";
-	    latex += to_latex(b);
-	    latex += "}";
+	    latex.push_str("\frac{");
+	    latex.push_str(&to_latex(a));
+	    latex.push_str("}{");
+	    latex.push_str(&to_latex(b));
+	    latex.push_str("}");
 	},
 	Operation::Pow(a, b) => {
-	    latex += to_latex(a);
-	    latex += "^{";
-	    latex += to_latex(b);
-	    latex += "}";
+	    latex.push_str(&to_latex(a));
+	    latex.push_str("^{");
+	    latex.push_str(&to_latex(b));
+	    latex.push_str("}");
 	},
+	Operation::Mod(a, b) => {
+	    latex.push_str(&to_latex(a));
+	    latex.push_str(" \mod ");
+	    latex.push_str(&to_latex(b));
+	}
     }
     return latex;
 }
