77683a106505a3377e4e4ede4f8cf74d2c9f70fc
Vihaan Jim <vjim@hs-student27612.princetonk12.org>
2022-11-05 11:40:39 -0400

    change to format

--- src/latex.rs
+++ src/latex.rs
@@ -4,40 +4,25 @@ pub fn to_latex(tree: &Box<Operation>) -> String {
     let mut latex = String::new();
     match tree {
 	Operation::Num(a) => {
-	    latex += format!("{}", a);
+	    latex = format!("{}", a);
 	}
 	Operation::Add(a, b) => {
-	    latex.push_str(&to_latex(a));
-	    latex.push_str("+");
-	    latex.push_str(&to_latex(b));
+	    latex = format!("{} + {}", to_latex(a), to_latex(b));
 	},
 	Operation::Subtract(a, b) => {
-	    latex.push_str(&to_latex(a));
-	    latex.push_str("-");
-	    latex.push_str(&to_latex(b));
+	    latex = format!("{} - {}", to_latex(a), to_latex(b));
 	},
 	Operation::Multiply(a, b) => {
-	    latex.push_str(&to_latex(a));
-	    latex.push_str(" \times ");
-	    latex.push_str(&to_latex(b));
+	    latex = format!("{} \\times {}", to_latex(a), to_latex(b));
 	},
 	Operation::Divide(a, b) => {
-	    latex.push_str("\frac{");
-	    latex.push_str(&to_latex(a));
-	    latex.push_str("}{");
-	    latex.push_str(&to_latex(b));
-	    latex.push_str("}");
+	    latex = format!("\\frac\{{}\}\{{}\}", to_latex(a), to_latex(b));
 	},
 	Operation::Pow(a, b) => {
-	    latex.push_str(&to_latex(a));
-	    latex.push_str("^{");
-	    latex.push_str(&to_latex(b));
-	    latex.push_str("}");
+	    latex = format!("{}^\{{}\}", to_latex(a), to_latex(b));
 	},
 	Operation::Mod(a, b) => {
-	    latex.push_str(&to_latex(a));
-	    latex.push_str(" \mod ");
-	    latex.push_str(&to_latex(b));
+	    latex = format!("{} \\mod {}", to_latex(a), to_latex(b));
 	}
     }
     return latex;
